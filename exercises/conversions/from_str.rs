b4d4f4950bf07f1a834562a30de58048b0ecbd11
Jacoe Guo <jacoesad@126.com>
2023-10-23 17:46:01 +0800

    Exercises Progress: 95/100
    [091] using_as
    [092] from_into
    [093] from_str
    [094] try_from_into
    [095] as_ref_mut

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +52,20 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return  Err(ParsePersonError::Empty);
+        }
+        let person: Vec<_> = s.split(",").collect();
+        if person.len() != 2 {
+            return  Err(ParsePersonError::BadLen);
+        }
+        if person[0].len() == 0 {
+            return  Err(ParsePersonError::NoName);
+        }
+        match person[1].parse::<usize>() {
+            Ok(a) => Ok(Person {name: person[0].to_string(), age: a}),
+            Err(e) => Err(ParsePersonError::ParseInt(e)),
+        }
     }
 }
 
